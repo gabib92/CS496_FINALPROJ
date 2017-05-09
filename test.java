c4d7813ed88d7c925cfa6851153b8b9bd916e8df
gabib92 <gabriellealyssa.gb@gmail.com>
2017-05-09 00:34:22 -0700

    Update test.java

--- test.java
+++ test.java
@@ -2,6 +2,8 @@ package main;
 
 import main.CSVReader;
 import main.CSVReader.Game;
+import main.Sorts;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
@@ -13,13 +15,29 @@ public class test {
 			PrintWriter writer = new PrintWriter(outFile, "UTF-8");
 			List<Game> results = LoadIncidentFromCSV(inFile);
 
-            writer.println("\nTop 5 and Low 5");
-            for(int i = 0; i < 5; i++){
-                writer.println((results.get(i)).toString());
+			writer.println("[Game] [Year] [Platform] [Genre] [Gobal Sales]");
+			
+            writer.println("\nTop 5 and Low 5 of global sales");
+            List<Game> bySales = Sorts.qsort(results, 0, results.size()-1, "sales");
+            for(int i = bySales.size()-1; i>bySales.size()-6; i--){
+                writer.println((bySales.get(i)).toString());
             }
             writer.println("...");
-            for(int i = results.size()-5; i<results.size(); i++) {
-                writer.println(results.get(i).toString());
+            for(int i = 5; i>0; i--) {
+                writer.println(bySales.get(i).toString());
+            }
+            
+            writer.println("\nTop 5 sales for each year");
+            //Changes the year for each sublist
+            for(int i = 1985; i < 1990; i++) {
+            	String num = Integer.toString(i);
+            	List<Game> byYearSub = CSVReader.subList(results, "year", num);
+            	writer.println(num);
+            	//Displays the top 5 sales for that year
+            	for(int j = byYearSub.size()-1; j>byYearSub.size()-6; j--) {
+            		List<Game> byYear = Sorts.qsort(byYearSub, 0, byYearSub.size()-1, "year");
+            		writer.println(byYear.get(j).toString());
+            	}
             }
             writer.close();
 		}
@@ -27,7 +45,6 @@ public class test {
 		catch (IOException e) {
             System.out.println("Could not write to file.");
         }
-	     
 	}
 	 
 	private static List<Game> LoadIncidentFromCSV(String filename) throws IOException {
@@ -41,24 +58,6 @@ public class test {
         }
         else {
             new test(args[0], args[1]);
-			try {
-				List<Game> results = LoadIncidentFromCSV(args[0]);
-				//results = CSVReader.qsort(results,(results.size()/2),results.size()-1,"year");
-				//results = CSVReader.sort(results, "sales");
-				results = CSVReader.subList(results, "genre", "Platform");
-				//Game temp = results.get(0);
-				//results.set(0, results.get(results.size()-1));
-				//results.set(results.size()-1, temp);
-	            for(int i = 0; i < results.size(); i++){
-	                System.out.println((results.get(i)).toString());
-	            }
-	            //System.out.println("...");
-	            //for(int i = results.size()-5; i<results.size(); i++) {
-	            //	System.out.println(results.get(i).toString());
-	            //}
-			} catch (IOException e) {
-				System.out.println("couldn't read file");
-			}
         }
 
         System.out.println("Complete!");
